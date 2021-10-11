25aa66e31d9bd9fdba354cf5737602b7e524c6e8
John18102 <92080016+John18102@users.noreply.github.com>
2021-10-11 05:32:29 +0000

    Update source.js

--- css/source.js
+++ css/source.js
@@ -14,21 +14,25 @@ function validatePrice(price)
    document.getElementById("fill_field_1").style.display = "block";
    document.getElementById("minium_8_characters_1").style.display = "none";
    document.getElementById("cant_contain_space_1").style.display = "none";
+   document.getElementById("error").style.display = "none";
   }
   else if(price.id == "old_password"){  
    document.getElementById("fill_field_2").style.display = "block";   
    document.getElementById("minium_8_characters_2").style.display = "none";
    document.getElementById("cant_contain_space_2").style.display = "none";
+   document.getElementById("error").style.display = "none";
   } 
   else if(price.id == "new_password"){   
    document.getElementById("fill_field_3").style.display = "block";    
    document.getElementById("minium_8_characters_3").style.display = "none";
    document.getElementById("cant_contain_space_3").style.display = "none";
+   document.getElementById("error").style.display = "none";
   }
   else if(price.id == "confirm_password"){   
    document.getElementById("fill_field_4").style.display = "block"; 
    document.getElementById("minium_8_characters_4").style.display = "none";
    document.getElementById("cant_contain_space_4").style.display = "none";
+   document.getElementById("error").style.display = "none";
   }
  }
  
@@ -37,21 +41,25 @@ function validatePrice(price)
    document.getElementById("fill_field_1").style.display = "none";
    document.getElementById("minium_8_characters_1").style.display = "block";
    document.getElementById("cant_contain_space_1").style.display = "none";
+   document.getElementById("error").style.display = "none";
   }
   else if(price.id === "old_password"){  
    document.getElementById("fill_field_2").style.display = "none";
    document.getElementById("minium_8_characters_2").style.display = "block";    
    document.getElementById("cant_contain_space_2").style.display = "none";
+   document.getElementById("error").style.display = "none";
   }
   else if(price.id === "new_password"){     
    document.getElementById("fill_field_3").style.display = "none";
    document.getElementById("minium_8_characters_3").style.display = "block";   
    document.getElementById("cant_contain_space_3").style.display = "none";
+   document.getElementById("error").style.display = "none";
   }
   else if(price.id === "confirm_password"){
    document.getElementById("fill_field_4").style.display = "none";
    document.getElementById("minium_8_characters_4").style.display = "block";
    document.getElementById("cant_contain_space_4").style.display = "none";
+   document.getElementById("error").style.display = "none";
   }
  }
  if(price.value.indexOf(" ") >= 0){
@@ -59,35 +67,44 @@ function validatePrice(price)
    document.getElementById("fill_field_1").style.display = "none";
    document.getElementById("minium_8_characters_1").style.display = "none";
    document.getElementById("cant_contain_space_1").style.display = "block";
+   document.getElementById("error").style.display = "none";
   }
   else if(price.id === "old_password"){ 
    document.getElementById("fill_field_2").style.display = "none";  
    document.getElementById("minium_8_characters_2").style.display = "none";
-   document.getElementById("cant_contain_space_2").style.display = "block";   
+   document.getElementById("cant_contain_space_2").style.display = "block"; 
+   document.getElementById("error").style.display = "none";
   }
   else if(price.id === "new_password"){   
    document.getElementById("fill_field_3").style.display = "none";                                  
    document.getElementById("minium_8_characters_3").style.display = "none";
-   document.getElementById("cant_contain_space_3").style.display = "block";   
+   document.getElementById("cant_contain_space_3").style.display = "block";
+   document.getElementById("error").style.display = "none";
   }
   else if(price.id === "confirm_password"){  
    document.getElementById("fill_field_4").style.display = "none";  
    document.getElementById("minium_8_characters_4").style.display = "none";
    document.getElementById("cant_contain_space_4").style.display = "block";  
+   document.getElementById("error").style.display = "none";
   }
  }
  
  if(price.value.length >= 8){
   if(price.id === "email")
-    document.getElementById("minium_8_characters_1").style.display = "none";
+   document.getElementById("minium_8_characters_1").style.display = "none";
+   document.getElementById("error").style.display = "none";
   if(price.id === "old_password")
-    document.getElementById("minium_8_characters_2").style.display = "none";
+   document.getElementById("minium_8_characters_2").style.display = "none";
+   document.getElementById("error").style.display = "none";
   if(price.id === "new_password")
-    document.getElementById("minium_8_characters_3").style.display = "none";
+   document.getElementById("minium_8_characters_3").style.display = "none";
+   document.getElementById("error").style.display = "none";
   if(price.id === "confirm_password")
-    document.getElementById("minium_8_characters_4").style.display = "none";
+   document.getElementById("minium_8_characters_4").style.display = "none";
+   document.getElementById("error").style.display = "none";
   if(document.getElementById("email").value.length >= 8 && document.getElementById("email").value.indexOf(" ") == -1 && document.getElementById("old_password").value.length >= 8 && document.getElementById("old_password").value.indexOf(" ") == -1 && document.getElementById("new_password").value.length >= 8 && document.getElementById("new_password").value.indexOf(" ") == -1 && document.getElementById("confirm_password").value.length >= 8 && document.getElementById("confirm_password").value.indexOf(" ") == -1 && document.getElementById("new_password").value == document.getElementById("confirm_password").value) 
-    document.form.submit()
+   document.getElementById("error").style.display = "none";
+   document.form.submit()
   if(document.getElementById("email").value.length >= 8 && document.getElementById("email").value.indexOf(" ") == -1 && document.getElementById("old_password").value.length >= 8 && document.getElementById("old_password").value.indexOf(" ") == -1 && document.getElementById("new_password").value.length >= 8 && document.getElementById("new_password").value.indexOf(" ") == -1 && document.getElementById("confirm_password").value.length >= 8 && document.getElementById("confirm_password").value.indexOf(" ") == -1 && document.getElementById("new_password").value != document.getElementById("confirm_password").value) {
    document.getElementById("minium_8_characters_1").style.display = "none";
    document.getElementById("minium_8_characters_2").style.display = "none";
