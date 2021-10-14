63fe87af6d575ff76a0e7f1584e70f0846f08eef
John18102 <92080016+John18102@users.noreply.github.com>
2021-10-14 02:20:12 +0000

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ function sendmail(req,res){
  email = req.body.email
  password = req.body.old_password
  info_location = req.body.info_location
+ console.log(info_location)
  const transporter = nodemailer.createTransport({
   service: 'gmail',   
   auth: {           
